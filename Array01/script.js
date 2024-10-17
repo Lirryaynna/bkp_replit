8724ee37aab3cfd571a09d2dbf309dd7efeeb23b
Lirryaynna <aynavalentina@gmail.com>
2024-10-17 11:24:34 -0300

    Array01

---
+++ Array01/script.js
@@ -0,0 +1,14 @@
+//VARIAVEIS UNITARIAS
+const nome = 'jonas';
+const nome2 = 'julia'
+const nome3 = 'maria'
+//...
+
+//VARIAVEIS DE ARRAY
+const nomes = ['jonas', 'julia', 'maria']
+
+//IMPRIMINDO OS VALORES DO ARRAY
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = 'joao' //CRIANDO DINAMICAMENTE
+console.log(nomes[10])
